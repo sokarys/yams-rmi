a1c7afb3178426dba2964f706076cd1eb89be134
Laurent.gadel@gmail.com <Laurent.gadel@gmail.com@1e2f9ef9-c6d9-6fe5-00b0-5f014a78d80b>
2011-05-26 08:44:24 +0000

    modèle + menu

--- src/ServeurJeu/modele/Main.java
+++ src/ServeurJeu/modele/Main.java
@@ -4,38 +4,137 @@
  */
 package ServeurJeu.modele;
 
+import ServeurJeu.modele.Regles.TYPESCORE;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map.Entry;
 
 /**
  *
  * @author root
  */
 public class Main implements Serializable{
-    private HashMap<Integer,De> Des;
+    private HashMap<Integer,De> des;
     private ArrayList<Integer> listeDesARelancer;
-    private Regles regle;
     
     public Main(){
-        Des = new HashMap<Integer,De>();
+        des = new HashMap<Integer,De>();
+        for(int i=0; i<5; i++){
+            des.put(i, new De());
+        }
         listeDesARelancer = new ArrayList<Integer>();
-        regle = new Regles();
     }
-    
+
+    public void MainCheat(int de, int face){
+        des.get(de).setFace(face);
+    }
+
     public void clearListDeARelancer(){
         listeDesARelancer.clear();
     }
-    
-    public void addDeARelancer(){
-        
+    public void relancerLesDes(){
+        for(Integer i : listeDesARelancer){
+            des.get(i).lancerLeDe();
+        }
+        listeDesARelancer.clear();
     }
-    
-    public void relancerDes(){
-        
+    public void choixDeARelancer(int index){
+        listeDesARelancer.add(index);
+    }
+
+    public HashMap<Integer, De> getDes() {
+        return des;
     }
     
     public Integer getScrore(Regles.TYPESCORE type){
         return 0;
+    }
+
+    @Override
+    public String toString(){
+        ArrayList<Integer> array = new ArrayList<Integer>();
+        for(Entry<Integer, De> currentEntry : des.entrySet()) {
+            array.add(currentEntry.getValue().getFace());
+        }
+        return array.toString();
+    }
+
+    public void afficherMain(){
+        System.out.println("┌-------┐ ┌-------┐ ┌-------┐ ┌-------┐ ┌-------┐");
+        String ligne1 = "";
+        String ligne2 = "";
+        String ligne3 = "";
+        for(Entry<Integer, De> currentEntry : des.entrySet()) {
+            switch(currentEntry.getValue().getFace()){
+                case 1 :
+                    ligne1 +="│       │ ";
+                    ligne2 +="│   •   │ ";
+                    ligne3 +="│       │ ";
+                    break;
+                case 2:
+                    ligne1 +="│ •     │ ";
+                    ligne2 +="│       │ ";
+                    ligne3 +="│     • │ ";
+                    break;
+                case 3 :
+                    ligne1 +="│ •     │ ";
+                    ligne2 +="│   •   │ ";
+                    ligne3 +="│     • │ ";
+                    break;
+                case 4 :
+                    ligne1 +="│ •   • │ ";
+                    ligne2 +="│       │ ";
+                    ligne3 +="│ •   • │ ";
+                    break;
+                case 5 :
+                    ligne1 +="│ •   • │ ";
+                    ligne2 +="│   •   │ ";
+                    ligne3 +="│ •   • │ ";
+                    break;
+                case 6 :
+                    ligne1 +="│ •   • │ ";
+                    ligne2 +="│ •   • │ ";
+                    ligne3 +="│ •   • │ ";
+                    break;
+            }
+        }
+        System.out.println(ligne1);
+        System.out.println(ligne2);
+        System.out.println(ligne3);
+        System.out.println("└-------┘ └-------┘ └-------┘ └-------┘ └-------┘");
+    }
+
+    public Integer getScore(Main m,TYPESCORE type,int X){
+        switch(type){
+            case AS :
+                return Regles.pointNombreUnique(m,1);
+            case DEUX :
+                return Regles.pointNombreUnique(m,2);
+            case TROIS :
+                return Regles.pointNombreUnique(m,3);
+            case QUATRE :
+                return Regles.pointNombreUnique(m,4);
+            case CINQ :
+                return Regles.pointNombreUnique(m,5);
+            case SIX :
+                return Regles.pointNombreUnique(m,6);
+            case SUPERIEUR :
+                return Regles.pointSuperieur(m,X);
+            case INFERIEUR :
+                return Regles.pointInferieur(m,X);
+            case CARRE :
+                return Regles.pointCarre(m);
+            case FULL :
+                return Regles.pointFull(m);
+            case PETITE_SUITE :
+                return Regles.pointPetiteSuite(m);
+            case GRANDE_SUITE :
+                return Regles.pointGrandeSuite(m);
+            case YAMS :
+                return Regles.pointYams(m);
+            default:
+                return -1;
+        }
     }
 }
--- src/ServeurJeu/modele/Regles.java
+++ src/ServeurJeu/modele/Regles.java
@@ -5,44 +5,211 @@
 package ServeurJeu.modele;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map.Entry;
 
 /**
  *
  * @author root
  */
 public class Regles implements Serializable{
+
     public static enum TYPESCORE {AS,DEUX,TROIS,QUATRE,CINQ,SIX,SUPERIEUR,INFERIEUR,CARRE,FULL,PETITE_SUITE,GRANDE_SUITE,YAMS};
-    
-    /*public Integer insertScore(Main m, FeuilleScore.TYPESCORE type,int X){
-        switch(type){
-            case AS :
-                return pointNombreUnique(m,1);
-            case DEUX :
-                return pointNombreUnique(m,2);
-            case TROIS :
-                return pointNombreUnique(m,3);
-            case QUATRE :
-                return pointNombreUnique(m,4); 
-            case CINQ :
-                return pointNombreUnique(m,5);
-            case SIX :
-                return pointNombreUnique(m,6);
-            case SUPERIEUR :
-                return pointSuperieur(m,X);
-            case INFERIEUR :
-                return pointInferieur(m,X);
-            case CARRE :
-                return pointCarre(m);
-            case FULL :
-                return pointFull(m);
-            case PETITE_SUITE :
-                return pointPetiteSuite(m);
-            case GRANDE_SUITE :
-                return pointGrandeSuite(m);     
-            case YAMS :
-                return PointYams(m);
-            default:
-                return -1;
+
+    //***********************************
+    //* Partie supérieure de la feuille
+    //***********************************
+    public static Integer pointNombreUnique(Main main,Integer nombre){
+        HashMap<Integer,De> des = main.getDes();
+        Integer nbElements = 0;
+        for (Entry<Integer, De> currentEntry : des.entrySet()) {
+            De de = currentEntry.getValue();
+            if(de.getFace() == nombre){
+                nbElements++;
+            }
         }
-    }*/
+        return nbElements*nombre;
+    }
+
+    //***********************************
+    //* Partie centrale de la feuille
+    //***********************************
+
+    public static Integer pointSuperieur(Main main, Integer pointsInferieur){
+        HashMap<Integer,De> des = main.getDes();
+        Integer somme = 0;
+        for(Entry<Integer, De> currentEntry : des.entrySet()) {
+            De de = currentEntry.getValue();
+            somme += de.getFace();
+        }
+        // -1 indique que la case "Inférieur" n'est pas encore remplie
+        if(pointsInferieur == -1){
+            return somme;
+        }else{
+            if(somme > pointsInferieur){
+                return somme;
+            }else{
+                return 0;
+            }
+        }
+    }
+
+    public static Integer pointInferieur(Main main, Integer pointsSuperieur){
+        HashMap<Integer,De> des = main.getDes();
+        Integer somme = 0;
+        for(Entry<Integer, De> currentEntry : des.entrySet()) {
+            De de = currentEntry.getValue();
+            somme += de.getFace();
+        }
+        // -1 indique que la case "Supérieur" n'est pas encore remplie
+        if(pointsSuperieur == -1){
+            return somme;
+        }else{
+            if(somme < pointsSuperieur){
+                return somme;
+            }else{
+                return 0;
+            }
+        }
+    }
+
+    //***********************************
+    //* Partie inférieure de la feuille
+    //***********************************
+
+    public static Integer pointCarre(Main main){
+        HashMap<Integer,De> des = main.getDes();
+        HashMap<Integer,Integer> occurences = new HashMap<Integer, Integer>();
+        Integer somme = 0;
+        //Intitialisation
+        for(int i=1; i<=6;i++){
+            occurences.put(i, 0);
+        }
+        //Remplissage
+        for(Entry<Integer, De> currentEntry : des.entrySet()) {
+            De de = currentEntry.getValue();
+            somme += de.getFace();
+            occurences.put(de.getFace(),occurences.get(de.getFace())+1);
+        }
+        //Vérification
+        for(Entry<Integer, Integer> currentEntry : occurences.entrySet()) {
+            Integer nbrOccurences = currentEntry.getValue();
+            if(nbrOccurences >= 4){
+                return 40 + somme;
+            }
+        }
+        return 0;
+    }
+
+    public static Integer pointFull(Main main){
+        HashMap<Integer,De> des = main.getDes();
+        HashMap<Integer,Integer> occurences = new HashMap<Integer, Integer>();
+        Integer somme = 0;
+        Boolean DeuxIdentiques = false;
+        Boolean TroisIdentiques = false;
+        //Intitialisation
+        for(int i=1; i<=6;i++){
+            occurences.put(i, 0);
+        }
+        //Remplissage
+        for(Entry<Integer, De> currentEntry : des.entrySet()) {
+            De de = currentEntry.getValue();
+            somme += de.getFace();
+            occurences.put(de.getFace(),occurences.get(de.getFace())+1);
+        }
+        //Vérification
+        for(Entry<Integer, Integer> currentEntry : occurences.entrySet()) {
+            Integer nbrOccurences = currentEntry.getValue();
+            if(nbrOccurences == 3){
+                TroisIdentiques = true;
+            }else if(nbrOccurences == 2){
+                DeuxIdentiques = true;
+            }
+        }
+        if(DeuxIdentiques && TroisIdentiques){
+            return 30 + somme;
+        }else{
+            return 0;
+        }
+    }
+
+    public static Integer pointPetiteSuite(Main main){
+        HashMap<Integer,De> des = main.getDes();
+        ArrayList<Integer> arrayDes = new ArrayList<Integer>();
+        for(Entry<Integer, De> currentEntry : des.entrySet()) {
+            De de = currentEntry.getValue();
+            if(!arrayDes.contains(de.getFace())){
+                arrayDes.add(de.getFace());
+            }
+        }
+        Collections.sort(arrayDes);
+        if(arrayDes.size() >= 4){
+            for(int i=1; i<arrayDes.size(); i++){
+                if(arrayDes.get(i) != arrayDes.get(i-1) + 1){
+                    return 0;
+                }
+            }
+            return 45;
+        }
+        return 0;
+    }
+
+    public static Integer pointGrandeSuite(Main main){
+        HashMap<Integer,De> des = main.getDes();
+        ArrayList<Integer> arrayDes = new ArrayList<Integer>();
+        for(Entry<Integer, De> currentEntry : des.entrySet()) {
+            De de = currentEntry.getValue();
+            if(!arrayDes.contains(de.getFace())){
+                arrayDes.add(de.getFace());
+            }
+        }
+        Collections.sort(arrayDes);
+        if(arrayDes.size() == 5){
+            for(int i=1; i<arrayDes.size(); i++){
+                if(arrayDes.get(i) != arrayDes.get(i-1) + 1){
+                    return 0;
+                }
+            }
+            return 50;
+        }
+        return 0;
+    }
+
+    public static Integer pointYams(Main main){
+        HashMap<Integer,De> des = main.getDes();
+        HashMap<Integer,Integer> occurences = new HashMap<Integer, Integer>();
+        Integer somme = 0;
+        //Intitialisation
+        for(int i=1; i<=6;i++){
+            occurences.put(i, 0);
+        }
+        //Remplissage
+        for(Entry<Integer, De> currentEntry : des.entrySet()) {
+            De de = currentEntry.getValue();
+            somme += de.getFace();
+            occurences.put(de.getFace(),occurences.get(de.getFace())+1);
+        }
+        //Vérification
+        for(Entry<Integer, Integer> currentEntry : occurences.entrySet()) {
+            Integer nbrOccurences = currentEntry.getValue();
+            if(nbrOccurences == 5){
+                return 50 + somme;
+            }
+        }
+        return 0;
+    }
+
+    /* Au Yahtzee uniquement
+    public Integer pointChance(Main main){
+        HashMap<Integer,De> des = main.getDes();
+        return 0;
+    }
+
+    public Integer pointBrelan(Main main){
+        HashMap<Integer,De> des = main.getDes();
+        return 0;
+    }
+    */
 }
